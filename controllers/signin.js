9a8d76c6e88b7d82dc6f14dc0f959998a11c6da9
Rabindra Joshi <therj@users.noreply.github.com>
2018-06-11 00:12:50 +0545

    deploy init

---
+++ controllers/signin.js
@@ -0,0 +1,34 @@
+const handleSignIn = (db, bcrypt) => (req, res) => {
+    const {
+        email,
+        password
+    } = req.body;
+
+    if (!email || !password) {
+        return res.status(400).json("Incorrect form Submission!")
+    }
+    db
+        .select('email', 'hash')
+        .where('email', '=', email)
+        .from('login')
+        .then(data => {
+            const isValid = bcrypt.compareSync(password, data[0].hash)
+            if (isValid) {
+                return db
+                    .select('*')
+                    .from('users')
+                    .where('email', '=', email)
+                    .then(user => {
+                        res.json(user[0])
+                    })
+                    .catch(err => res.status(400).json('Unable to get User'))
+            } else {
+                res.status(400).json('Incorrect Credentials!')
+            }
+        })
+        .catch(err => res.status(400).json('Incorrect Credentials!'))
+}
+
+module.exports = {
+    handleSignIn: handleSignIn,
+}
---
+++ server.js
@@ -0,0 +1,46 @@
+const PORT = process.env.PORT || 3000===]
+const bcrypt = require('bcrypt-nodejs')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const express = require('express')
+const pg = require('pg')
+const knex = require('knex')
+const register = require('./controllers/register')
+const signin = require('./controllers/signin')
+const profile = require('./controllers/profile')
+const findface = require('./controllers/findface')
+app = express()
+  .use(cors())
+  .use(express.static(__dirname + '/public'))
+  .use(bodyParser.json())
+  .use(
+    bodyParser.urlencoded({
+      extended: false,
+    }),
+  )
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host: '127.0.0.1',
+    user: 'postgres',
+    password: 'postgres',
+    database: 'magicbrain',
+  },
+})
+
+app.get('/', (req, res) => {
+  res.json('It is working!')
+})
+
+app.post('/signin', signin.handleSignIn(db, bcrypt))
+
+app.post('/register', register.handleRegister(db, bcrypt))
+app.get('/profile/:id', profile.handleProfileGet(db))
+
+app.put('/findface', findface.handleImage(db))
+app.post('/imageUrl', findface.handleApiCall())
+
+app.listen(PORT, () => {
+  console.log(`Server Running on port ${PORT} !`)
+})
